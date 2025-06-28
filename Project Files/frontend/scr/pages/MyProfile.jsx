87181a00878d4aafbe07278d9f7d2330de89f8fb
TAKSH752 <yallamgarigopikrishna@gmail.com>
2025-06-28 18:55:58 +0530

    Create MyProfile.jsx

---
+++ Project Files/frontend/scr/pages/MyProfile.jsx
@@ -0,0 +1,16 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { assets } from '../assets/assets'
+
+const MyProfile = () => {
+
+    const [isEdit, setIsEdit] = useState(false)
+
+    const [image, setImage] = useState(false)
+
+    const { token, backendUrl, userData, setUserData, loadUserProfileData } = useContext(AppContext)
+
+    // Function to update user profile data using API
+    const updateUserProfileData = async () => {
