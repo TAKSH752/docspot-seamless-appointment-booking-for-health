d89007de55a6f5164180d6bf27f974e0c19ee98e
TAKSH752 <yallamgarigopikrishna@gmail.com>
2025-06-28 18:41:40 +0530

    Create Banner.jsx

---
+++ Project Files/frontend/scr/assets/components/Banner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+
+const Banner = () => {
+
+    const navigate = useNavigate()
+
+    return (
+        <div className='flex bg-primary rounded-lg  px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10'>
